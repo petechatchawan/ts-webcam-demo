96cd3dfefd20909011ba58f8340021f4dc50e8a7
Chatchawan Kredsawat <pete.chatchawan@gmail.com>
2025-03-15 22:42:30 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ts-webcam-demo',
+  webDir: 'www'
+};
+
+export default config;
