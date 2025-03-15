e1df1c897f6af142f3f1baa4e2070af8a91f079f
Chatchawan Kredsawat <pete.chatchawan@gmail.com>
2025-03-16 01:32:09 +0700

    updated fix bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
     selector: 'app-root',
     templateUrl: 'app.component.html',
-    imports: [IonApp, IonRouterOutlet],
+    imports: [IonicModule],
 })
 export class AppComponent {
     constructor() {}
